5c87ca88de8e9e9bf014b8afa93299718f8ad57f
karan_patel <karankantesariya123@gmail.com>
2020-04-06 12:36:38 +0530

    Edit in home blade file

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,6 +16,15 @@
 
                     You are logged in!
                 </div>
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    You are logged in!
+                </div>
             </div>
         </div>
     </div>
